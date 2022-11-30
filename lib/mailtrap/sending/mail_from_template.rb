8c55de08bbe020a47f533b516f06f1559c52d3c8
Andriy Yanko <andriy.yanko@gmail.com>
2022-11-30 20:42:07 +0200

    Add Mailtrap::Sending::MailFromTemplate

---
+++ lib/mailtrap/sending/mail_from_template.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Mailtrap
+  module Sending
+    class MailFromTemplate < Base
+      attr_accessor :template_uuid, :template_variables
+
+      def initialize( # rubocop:disable Metrics/ParameterLists, Metrics/MethodLength
+        from: nil,
+        to: [],
+        cc: [],
+        bcc: [],
+        attachments: [],
+        headers: {},
+        custom_variables: {},
+        template_uuid: nil,
+        template_variables: {}
+      )
+        super(
+          from: from,
+          to: to,
+          cc: cc,
+          bcc: bcc,
+          attachments: attachments,
+          headers: headers,
+          custom_variables: custom_variables
+        )
+        @template_uuid = template_uuid
+        @template_variables = template_variables
+      end
+
+      def as_json
+        super.merge(
+          {
+            'template_uuid' => template_uuid,
+            'template_variables' => template_variables
+          }
+        ).compact
+      end
+    end
+  end
+end
---
+++ spec/mailtrap/sending/shared.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'with attachments' do
+  describe '#attachments=' do
+    subject(:attachments_list) { mail.attachments }
+
+    let(:attachments) { [{ content: StringIO.new('hello world'), filename: 'attachment.txt' }] }
+
+    its(:size) { is_expected.to eq(1) }
+
+    describe 'attachment_params' do
+      subject(:attachment) { attachments_list.first }
+
+      it { is_expected.to be_a(Mailtrap::Sending::Attachment) }
+      its(:content) { is_expected.to eq('aGVsbG8gd29ybGQ=') }
+      its(:filename) { is_expected.to eq('attachment.txt') }
+    end
+  end
+
+  describe '#add_attachment' do
+    subject(:add_attachment) { mail.add_attachment(**attachment) }
+
+    let(:attachment) { { content: StringIO.new('hello world'), filename: 'attachment.txt' } }
+
+    it 'adds an attachment' do
+      expect { add_attachment }.to change { mail.attachments.size }.from(0).to(1)
+
+      expect(mail.attachments.last.content).to eq('aGVsbG8gd29ybGQ=')
+    end
+  end
+end
