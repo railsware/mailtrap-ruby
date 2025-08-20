15a5b5aef5de53951efac1c0acfe40f92fec88ad
Jurgen <jurgen@kodolabs.com>
2025-08-20 21:41:19 +0200

    remove constructor, fix attribute type

--- lib/mailtrap/project.rb
+++ lib/mailtrap/project.rb
@@ -3,9 +3,9 @@
 module Mailtrap
   # Data Transfer Object for Project
   # @see https://api-docs.mailtrap.io/docs/mailtrap-api-docs/ee252e413d78a-create-project
-  # @attr_reader id [String] The project ID
+  # @attr_reader id [Integer] The project ID
   # @attr_reader name [String] The project name
-  # @attr_reader share_links [Array] Array of links
+  # @attr_reader share_links [Hash] Admin and viewer share links
   # @attr_reader inboxes [Array] Array of inboxes
   # @attr_reader permissions [Hash] List of permissions
   Project = Struct.new(
@@ -16,9 +16,6 @@ module Mailtrap
     :permissions,
     keyword_init: true
   ) do
-    def initialize(options)
-      super
-    end
 
     # @return [Hash] The project attributes as a hash
     def to_h
